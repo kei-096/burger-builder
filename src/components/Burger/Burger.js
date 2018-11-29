e492457d804e18bd8bcd9e3ee1a61a33d9bed788
Keitaro <fe08108@gmail.com>
2018-11-29 14:13:24 +0700

    insert if statement to check if ingredients are added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,14 +7,19 @@ const burger = (props) => {
     console.log(props)
     let result = Object.keys( props.ingredients )
     console.log(result)
-    const transformedIngredients = Object.keys( props.ingredients )
+    let transformedIngredients = Object.keys( props.ingredients )
         .map( igKey => {
             console.log(igKey)
             return [...Array( props.ingredients[igKey] )].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
         } )
-    console.log(transformedIngredients)
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
