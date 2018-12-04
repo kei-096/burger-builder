e28c2f7ccf388034dc3610957e99b0352c20669d
Keitaro <fe08108@gmail.com>
2018-12-04 15:29:06 +0700

    add drawer toggle

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,11 +3,12 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo height="80%" />
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <Logo height="80%"  />
         <nav className={classes.DesktopOnly}>
             <NavigationItems />
         </nav>
